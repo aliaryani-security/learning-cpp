66cecd0bd0c516ea5fb959a93c5773b1a169660c
Ali Aryani <lenesis@proton.me>
2025-08-04 10:34:30 -0400

    Output and comments

---
+++ 01-cpp-output/cpp-output.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    cout << "Hello World!";
+    cout << "I'm learning C++";
+    // the result will be these to lines joined together!
+    // cout can also print numbers :
+    cout << 3;
+    // math can also be done here :
+    cout << 12 + 3;
+    // let's talk about new lines
+    // there are two ways to add a new line:
+    cout << "Hello\n";
+    cout << "Hello" << "\n";
+    // this is not the end, there's another way!
+    cout << "https://github.com/aliaryani-security" << endl;
+    // This line is a comment. You know what it is.
+    /* And this, this is a 
+    mulitiline comment.
+    beautiful, isn't it?*/
+    return 0;
+}
