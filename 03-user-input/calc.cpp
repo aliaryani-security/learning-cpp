d60135980fb5b951fe3863b6024e368606dcb774
Ali Aryani <lenesis@proton.me>
2025-08-06 20:50:56 +0330

    simple calculator

---
+++ 03-user-input/calc.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int x,y,sum;
+    cout << "Enter first number: ";
+    cin >> x;
+    cout << "Enter second number: ";
+    cin >> y;
+    sum = x + y;
+    cout << "the sum: " << sum << endl;
+    return 0;
+}
