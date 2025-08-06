9da3a5a0e33e66d154fa6e237b10ca35c3583307
Ali Aryani <lenesis@proton.me>
2025-08-06 20:38:26 +0330

    user input intro

---
+++ 03-user-input/user-input.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int x;
+    cout << "Enter a number: ";
+    cin >> x; // get usre input from keyboard
+    cout << "You chose: " << x;
+    return 0;
+}
