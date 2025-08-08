6d55d45855970efb356a9fba1e45d408499bdc38
Ali Aryani <lenesis@proton.me>
2025-08-08 20:57:06 +0330

    operators

---
+++ 05-operators/ops.cpp
@@ -0,0 +1,20 @@
+
+int main () {
+    int x = 10;
+    int y = 6;
+    int num_1 = x + y; //addition
+    int num_2 = x - y; //subtraction
+    int num_3 = x * y; //multiplication
+    int num_4 = x / y; //division
+    int num_5 = x % y; //modulus
+    int num_6 = ++x; //increment
+    int num_7 = --y; //decrement
+    x += 5;
+    y -= 5;
+    y *= 2;
+    x /= 2;
+    x %= 3;
+    // some I don't understand right now, so I don't write them !
+
+    return 0;
+}
