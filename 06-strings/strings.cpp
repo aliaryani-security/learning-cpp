15626974912070df9eff00d5d05b55405ce7e45d
Ali Aryani <lenesis@proton.me>
2025-08-11 15:28:05 +0330

    access strings

--- 06-strings/strings.cpp
+++ 06-strings/strings.cpp
@@ -20,6 +20,15 @@ int main() {
     string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     cout << "the length of this string is: " << alphabet.length() << endl;
     // you can also use alphabet.size() and it will do the same thing for you.
+
+    // access strings
+    string my_hello = "Hello";
+    cout << my_hello[0] << endl;
+    // or to print the last character of the string
+    cout << my_hello[my_hello.length() - 1] << endl;
+    // see also:
+    my_hello[0] = 'J';
+    cout << my_hello << endl;
     return 0;
 
 }
