9ae0f43e91db4694dbd83e8c503947f33bb4bb96
Ali Aryani <lenesis@proton.me>
2025-08-14 19:46:45 +0330

    max and min

---
+++ 07-math/cpp-math.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+	// max() is used to find the highest value
+	cout << max(5,17) << endl;
+	
+	// min() is used to find the lowest value
+	cout << min (5,17) << endl;
+	return 0;
+}
