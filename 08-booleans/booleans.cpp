54906462fc63945ee59964c6d08853c2c2e5e003
Ali Aryani <lenesis@proton.me>
2025-08-14 20:33:27 +0330

    add bool example

--- 08-booleans/booleans.cpp
+++ 08-booleans/booleans.cpp
@@ -6,5 +6,16 @@ int main () {
 	bool sadness {false};
 	cout << happiness << "\n" << sadness << endl;
 	// 1 is true and 0 is false
+	
+	// real world example
+	int my_age {24};
+	int voting_age {18};
+	
+	if (my_age >= voting_age) {
+		    cout << "Old enough to vote!" << endl;
+		} else {
+			cout << "Not old enough to vote" << endl;
+		}
+	
 	return 0;
 }
