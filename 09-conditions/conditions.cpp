c43d3d9ab9a3dafb31d8312c8c7e29c2c7757f61
Ali Aryani <lenesis@proton.me>
2025-08-14 21:08:29 +0330

    if else

---
+++ 09-conditions/conditions.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+	// if
+	if ( 5 > 3) {
+		cout << "it was greater" << endl;
+	}
+	// if else
+	if (17 < 20){
+		cout << "it was smaller" << endl;
+	} else {
+		cout << "it was bigger" << endl;
+	}
+	
+	return 0;
+}
