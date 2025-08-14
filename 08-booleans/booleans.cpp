abd590e11c063bbe367366dd8e9aea46e1701971
Ali Aryani <lenesis@proton.me>
2025-08-14 20:18:38 +0330

    booleans

---
+++ 08-booleans/booleans.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+	bool happiness {true};
+	bool sadness {false};
+	cout << happiness << "\n" << sadness << endl;
+	// 1 is true and 0 is false
+	return 0;
+}
