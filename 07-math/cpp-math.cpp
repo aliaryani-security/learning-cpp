e7baad56d01bb62e96052c7044f4cdcc7af0b1cc
Ali Aryani <lenesis@proton.me>
2025-08-14 19:53:22 +0330

    basic cmath

--- 07-math/cpp-math.cpp
+++ 07-math/cpp-math.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+// include cmath library for more functions
+#include <cmath>
 using namespace std;
 
 int main () {
@@ -7,5 +9,13 @@ int main () {
 	
 	// min() is used to find the lowest value
 	cout << min (5,17) << endl;
+	
+	// from cmath library
+	// square root
+	cout << sqrt (64) << endl;
+	// round
+	cout << round (45.6) << endl;
+	// logarithm
+	cout << log (2) << endl;
 	return 0;
 }
