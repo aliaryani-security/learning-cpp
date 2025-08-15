3743ca96ae6171e78211c7ba07c08d375d348fcd
Ali Aryani <lenesis@proton.me>
2025-08-15 06:21:45 +0330

    if else examples

--- 09-conditions/conditions.cpp
+++ 09-conditions/conditions.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main () {
@@ -12,6 +13,33 @@ int main () {
 	} else {
 		cout << "it was bigger" << endl;
 	}
+	
+	// short hand if else
+	// this is also known as ternary
+	// this is used for simple conditions
+	//syntax :
+	// var = (condition) ? expression_true : expression_false;
+	int time {20};
+	string result = (time < 18) ? "Good day!" : "Good evening!";
+	cout << result << endl;
+	
+	// example 1
+	int door_code {1337};
+	if (door_code == 1337) {
+		cout << "Correct" << endl;
+	} else {
+		cout << "Incorrect" << endl;
+	}
+	// example 2
+	int my_num {10};
+	if (my_num > 0) {
+		cout << "Positive number" << endl;
+	} else if (my_num < 0) {
+		cout << "Negative number" << endl;
+	} else {
+		cout << "The number is zero" << endl;
+	}
+	
 	
 	return 0;
 }
