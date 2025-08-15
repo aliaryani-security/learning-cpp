f4a3c0b5834733b2a50b1b3f0f156ccfb8deaa46
Ali Aryani <lenesis@proton.me>
2025-08-15 06:42:51 +0330

    cpp switch

---
+++ 10-switch/cpp-switch.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+	// switch is to execute one matching block
+	int day {4};
+	switch (day) {
+		case 1:
+		    cout << "Saturday" << endl;
+		    break;
+		case 2:
+		    cout << "Sunday" << endl;
+		    break;
+		case 3:
+		    cout << "Monday" << endl;
+		    break;
+		case 4:
+		    cout << "Tuesday" << endl;
+		    break;
+		case 5:
+		    cout << "Wednesday" << endl;
+		    break;
+		case 6:
+		    cout << "Thursday" << endl;
+		    break;
+		case 7:
+		    cout << "Friday" << endl;
+		    break;
+		default:
+		    cout << "You got out of the matrix!";
+	}
+	
+	return 0;
+	
+}
