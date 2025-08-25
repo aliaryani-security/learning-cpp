1a1f87e56bde5ec82e3d204f8789b1db51ca0e47
Ali Aryani <lenesis@proton.me>
2025-08-25 19:01:05 +0330

    named structs

--- 13-structures/structures.cpp
+++ 13-structures/structures.cpp
@@ -2,6 +2,15 @@
 #include <string>
 using namespace std;
 
+// named structs
+// if you give an struct a name, 
+// you can treat it like a data type
+struct car {
+    string brand;
+    string model;
+    int year;
+};
+
 int main () {
     struct {
         int my_number;
@@ -21,5 +30,15 @@ int main () {
         int age;
         string name;
     } user_1, user_2, user_3;
+
+    // moved named struct outside main()
+    
+    car my_car;
+    my_car.brand = "BMW";
+    my_car.model = "X5";
+    my_car.year = 1999;
+
+    cout << my_car.brand << " " << my_car.model << " " << my_car.year << endl;
+
     return 0;
 }
