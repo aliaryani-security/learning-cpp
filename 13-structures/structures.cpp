a857b920fab7098eb06989e8df468720a20fcefb
Ali Aryani <lenesis@proton.me>
2025-08-25 18:51:39 +0330

    assign and access values

--- 13-structures/structures.cpp
+++ 13-structures/structures.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main () {
@@ -6,5 +7,13 @@ int main () {
         int my_number;
         string my_string;
     } my_structure; // structure name
+
+    // assign values :
+    my_structure.my_number = 10;
+    my_structure.my_string = "Hello";
+
+    // accessing values
+    cout << my_structure.my_number << endl;
+    cout << my_structure.my_string << endl;
     return 0;
 }
