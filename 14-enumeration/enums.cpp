4e15d98856df52b2946be50dbf45c49f47fedae5
Ali Aryani <lenesis@proton.me>
2025-08-26 07:59:19 +0330

    change default enum values

--- 14-enumeration/enums.cpp
+++ 14-enumeration/enums.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <string>
+// #include <string>
 using namespace std;
 
 int main () {
@@ -19,6 +19,16 @@ int main () {
     cout << my_var << endl; // returns 1.
     // by default, LOW = 0, MEDIUM = 1, HIGH = 2
 
+    // change values
+    // to change the default values
+    // do this :
+    enum scores {
+        FIRST = 100
+        , SECOND = 50
+        , THIRD = 25
+    };
+    enum scores my_score = FIRST;
+    cout << my_score << endl;
 
     return 0;
 }
