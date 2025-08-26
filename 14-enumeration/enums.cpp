fdbe96d8042054150ec5714dc9528d71aa48a426
Ali Aryani <lenesis@proton.me>
2025-08-26 07:49:20 +0330

    enum intro

---
+++ 14-enumeration/enums.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main () {
+    // enum is a special type that 
+    // represents a group of constants (unchangable values)
+    // it's considered a best practice to
+    // write items with upper case
+    enum level {
+        LOW
+        , MEDIUM
+        , HIGH
+    };
+
+    return 0;
+}
