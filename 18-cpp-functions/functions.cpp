a93802f47957331ee809323314f6a281a585fa32
Ali Aryani <lenesis@proton.me>
2025-09-04 08:53:02 +0330

    optimised conding

--- 18-cpp-functions/functions.cpp
+++ 18-cpp-functions/functions.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
 using namespace std
 
-; void my_function () {
-    cout << "I just got executed!" << endl
+; void my_function () { // decelaration
+    cout << "I just got executed!" << endl // defenition
+; }
+/*
+    if a function is declared after the main(),
+    you will get an error. howvever for better 
+    optimization, we can declare the function 
+    up here, and define it down there !
+*/
+void second_function()
+
+; int main () {
+    my_function()
+    // a function can be called multiple times
+    ; my_function()
+    ; my_function()
+    ; second_function()
+    ; return 0
 ; }
 
-int main () {
-    my_function()
-    ; return 0
+// here we go :
+void second_function() {
+    cout << "second function"
 ; }
